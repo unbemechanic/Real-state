772951a6452e88b654e394aad751223292429e43
Ilyosjon <the.mechanic99@bk.ru>
2024-09-06 16:03:39 +0900

    registration is complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import SignIn from "./page/signIn"
 import SignOut from "./page/signOut"
 import About from "./page/about"
 import Header from "./components/header"
+import Register from "./page/register"
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Header/>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/register" element={<Register/>}/>
         <Route path="/sign-in" element={<SignIn/>} />
         <Route path="/sign-out" element={<SignOut/>} />
         <Route path="/about" element={<About/>} />
---
+++ src/page/register.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+export default function Register() {
+    const [formData, setFormData] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value
+        })
+    }
+    // console.log(formDatas)
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true)
+        try {
+            const res = await fetch('http://localhost:5000/server/auth/register', {
+            method: 'POST',
+            headers:{
+                "Content-Type":"application/json",
+            },
+            body: JSON.stringify(formData)
+            });
+        const data = await res.json();
+        if(data.success === false){
+            setError(errror.message);
+            setLoading(false);
+            return
+        }
+        setLoading(false)
+        setError(null);
+        navigate('/sign-in')
+        console.log(data)
+        } catch (error) {
+            setLoading(false)
+            console.error(error)
+        }
+        
+    }
+
+  return (
+    <div className='p-3 max-w-lg mx-auto gap-3'>
+        <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4 '>
+            <input type="text" placeholder='Username' id='username' className='border rounded p-3'  onChange={handleChange} />
+            <input type="email" placeholder='Email' id='email' className='border rounded p-3' onChange={handleChange} />
+            <input type="password" placeholder='Password' id='password' className='border rounded p-3'  onChange={handleChange} />
+            <button  disabled={loading} type='submit' className='uppercase bg-slate-700 text-white p-3 rounded hover:opacity-95 disabled:opacity-80 active:bg-slate-600'>{loading ? 'Loading...':'Sign up'}</button>
+        </form>
+        <div className='flex gap-2 mt-5'>
+            <p>Have an account?</p>
+            <Link to={"/sign-in"}><span className='text-blue-700'>Sign in</span></Link>
+        </div>
+    </div>
+  )
+}
